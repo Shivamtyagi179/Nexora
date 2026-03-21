a57efeb91eb2e855f055b78467672108c4df9743
Shivamtyagi179 <shivamtyagi6226@gmail.com>
2026-03-21 17:43:17 +0530

    Delete Nexora.py

--- Nexora.py
+++
@@ -1,9 +0,0 @@
-from models.llm import LLM
-
-if __name__ == "__main__":
-    llm = LLM()
-    
-    while True:
-        user_input = input("You: ")
-        response = llm.generate(user_input)
-        print("Nexora:", response)
